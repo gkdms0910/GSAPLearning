bf9a966a8bd923d0d8f480d3c0b377e15cd126c6
gkdms0910 <73287609+gkdms0910@users.noreply.github.com>
2022-12-01 22:44:07 +0900

    home

---
+++ home.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+    TweenMax.to(document.documentElement, 4, {
+        "--split": "50%", 
+        repeat:-1, 
+        repeatDelay:1,
+        yoyo:true,
+       ease: "power4.inOut"
+      });
+}
